ebc8d00eccd368bb2dbc85f8b43439bb83559011
Brian Tochterman <btoch23@gmail.com>
2024-01-16 13:02:51 -0600

    file upload

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ var app = express();
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -44,6 +45,7 @@ app.use(passport.session());
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
+app.use('/imageUpload', uploadRouter);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
